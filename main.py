33a00954613aac3cca21f715e749859319f570ce
2PLCode <albie_p@icloud.com>
2025-05-20 20:30:51 +0100

    Hello, World!

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyscript import *
 from js import document, window
-display("Hello World!")
 
 def greet(event):
     document.getElementById("greeting").innerHTML = "Hello, " + document.getElementById("name").value + "!"
